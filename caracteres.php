14dc8396d975d4a459ef28578c036b1f12b01b1d
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2019-05-20 15:30:01 -0500

    Update caracteres.php
    Change the execution by blocks, changing step by step, removing accents from vowels.

--- caracteres.php
+++ caracteres.php
@@ -1,14 +1,18 @@
+<?php
+
 function caracteres($string = '')
 {
 
     $string = trim($string);
 
+    /*
     $string = str_replace(
         array("á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹"),
         array ("a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E"),
         $string
     );
-   /* $string = str_replace(
+    */
+    $string = str_replace(
         array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
         array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
         $string
@@ -27,13 +31,15 @@ function caracteres($string = '')
         array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
         array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
         $string
-    );*/
+    );
     $string = str_replace(
         array('ñ', 'Ñ', 'ç', 'Ç', '¥', '#', '¥'),
         array('n', 'N', 'c', 'C', 'N', 'N', 'N'),
         $string
     );
+    
     //Esta parte se encarga de eliminar cualquier caracter extraño
+    //This part handles other characters
     $string = str_replace(
 
         array("¡", "!", "", "¿", "?", "(", ")", "[", "]", "{", "}", "`",
@@ -50,3 +56,5 @@ function caracteres($string = '')
 
     return $string;
 }
+
+?>
